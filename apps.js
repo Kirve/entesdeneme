06f9905bbe75300ff8a0cca8b585f5b4643efaae
Kirve <serdar.akmehmet@gmail.com>
2019-03-12 14:34:25 +0200

    current and wanted location commit

--- apps.js
+++ apps.js
@@ -7,19 +7,19 @@ window.addEventListener('load', ()=> {
     let degree = document.querySelector('.temperature span');
     
     
+    
     if(navigator.geolocation){
-        let userLongitude = document.getElementById("loong").nodeValue;
-        let userLatitude = document.getElementById("loong").nodeValue;
+        
         navigator.geolocation.getCurrentPosition(position =>{
             
             
             longi = position.coords.longitude;
-            console.log(longi);
             lati = position.coords.latitude;
+           
             
             
             const proxy = "https://cors-anywhere.herokuapp.com/"
-            const api = proxy + "https://api.darksky.net/forecast/5951716312f6542a505fb5c9bccffc33/" + lati + "," + longi;
+            const api = proxy + "https://api.darksky.net/forecast/e75ed165e1cd8192e50424e97b9a6948/" + lati + "," + longi;
 
             fetch(api)
             .then( response =>{
@@ -32,7 +32,7 @@ window.addEventListener('load', ()=> {
                 let celsius = (temperature -32) * (5)/(9);
                 temperatureDegree.textContent = Math.floor(celsius); 
                 temperatureDescription.textContent = summary;
-                pressureLevel.textContent = pressure;
+                pressureLevel.textContent = pressure + " mb";
                 locationTimezone.textContent = data.timezone;
                 degree.textContent="C";
 
@@ -52,3 +52,112 @@ window.addEventListener('load', ()=> {
     }
   
 });
+
+document.getElementById("currentLoca").addEventListener('click', ()=> {
+
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    let pressureLevel = document.querySelector('.pressure-level');
+    let degree = document.querySelector('.temperature span');
+    
+    
+    
+    if(navigator.geolocation){
+        
+        navigator.geolocation.getCurrentPosition(position =>{
+            
+            
+            longi = position.coords.longitude;
+            lati = position.coords.latitude;
+           
+            
+            
+            const proxy = "https://cors-anywhere.herokuapp.com/"
+            const api = proxy + "https://api.darksky.net/forecast/e75ed165e1cd8192e50424e97b9a6948/" + lati + "," + longi;
+
+            fetch(api)
+            .then( response =>{
+                return response.json();
+            })
+            .then(data =>{
+                
+                const {temperature, summary, pressure, icon } = data.currently;
+
+                let celsius = (temperature -32) * (5)/(9);
+                temperatureDegree.textContent = Math.floor(celsius); 
+                temperatureDescription.textContent = summary;
+                pressureLevel.textContent = pressure + " mb";
+                locationTimezone.textContent = data.timezone;
+                degree.textContent="C";
+
+                setIcons(icon, document.querySelector('.icon'));
+
+                
+            });
+        });
+
+    }
+  
+    function setIcons(icon,iconID){
+        const skycons = new Skycons({color:"white"});
+        const currentIcon = icon.replace(/-/g,"_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
+  
+});
+
+document.getElementById("wantedLoca").addEventListener('click', ()=> {
+    
+    if(document.getElementById("loong").value=="" || document.getElementById("latit").value==""){
+        alert("Please enter correct longitude and latitude.")
+        
+    }
+    else{
+
+let temperatureDescription = document.querySelector('.temperature-description');
+let temperatureDegree = document.querySelector('.temperature-degree');
+let locationTimezone = document.querySelector('.location-timezone');
+let pressureLevel = document.querySelector('.pressure-level');
+let degree = document.querySelector('.temperature span');
+
+        lati = document.getElementById("loong").value;
+        longi = document.getElementById("latit").value;
+       
+        
+        const proxy = "https://cors-anywhere.herokuapp.com/"
+        const api = proxy + "https://api.darksky.net/forecast/e75ed165e1cd8192e50424e97b9a6948/" + lati + "," + longi;
+
+        fetch(api)
+        .then( response =>{
+            return response.json();
+        })
+        .then(data =>{
+            
+            const {temperature, summary, pressure, icon } = data.currently;
+
+            let celsius = (temperature -32) * (5)/(9);
+            temperatureDegree.textContent = Math.floor(celsius); 
+            temperatureDescription.textContent = summary;
+            pressureLevel.textContent = pressure + "mb";
+            locationTimezone.textContent = data.timezone;
+            degree.textContent="C";
+
+            setIcons(icon, document.querySelector('.icon'));
+
+            
+        });
+    
+
+
+
+        function setIcons(icon,iconID){
+            const skycons = new Skycons({color:"white"});
+            const currentIcon = icon.replace(/-/g,"_").toUpperCase();
+            skycons.play();
+            return skycons.set(iconID, Skycons[currentIcon]);
+        }
+    }
+    });
+
